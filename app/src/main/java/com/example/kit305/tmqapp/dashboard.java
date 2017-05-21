850c8ef2d13a7c644ad67fccd7aee5bd9a0ccb27
kit305 <kit305@macl-14.cis.utas.edu.au>
2017-05-21 10:48:07 +1000

    Initial commit

---
+++ app/src/main/java/com/example/kit305/tmqapp/dashboard.java
@@ -0,0 +1,13 @@
+package com.example.kit305.tmqapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
---
+++ app/src/main/java/com/example/kit305/tmqapp/new_task.java
@@ -0,0 +1,13 @@
+package com.example.kit305.tmqapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class new_task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task);
+    }
+}
